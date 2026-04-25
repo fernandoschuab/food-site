335296c2e5da2b6ad605ff14dde194a7b768f95c
Fernando Schuab <fschuab@gmail.com>
2026-04-24 21:58:43 -0300

    refactor: update JSX class attributes to className and implement auto-scroll to top on route change

--- app/components/Layouts/Layout.jsx
+++ app/components/Layouts/Layout.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
 function Layout({ children }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
